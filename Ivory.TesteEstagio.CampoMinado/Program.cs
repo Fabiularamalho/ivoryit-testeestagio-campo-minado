e72ccac7ae2bab6d39258ec46152b585aeed1d2c
Fabiula Araujo de Souza Ramalho <125487344+Fabiularamalho@users.noreply.github.com>
2023-06-27 22:49:25 -0300

    Resultado do teste

--- Ivory.TesteEstagio.CampoMinado/Program.cs
+++ Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -12,6 +12,46 @@ namespace Ivory.TesteEstagio.CampoMinado
 
             // Realize sua codificação a partir deste ponto, boa sorte!
 
+   
+
+        static void JogarCampoMinado(CampoMinado campoMinado)
+        {
+            while (campoMinado.JogoStatus == 0)
+            {
+                // Lógica para jogar o Campo Minado de forma autônoma
+                bool jogadaRealizada = false;
+
+                for (int linha = 0; linha < 9; linha++)
+                {
+                    for (int coluna = 0; coluna < 9; coluna++)
+                    {
+                        if (campoMinado.Jogo[linha, coluna] == "-")
+                        {
+                            campoMinado.Abrir(linha + 1, coluna + 1);
+                            jogadaRealizada = true;
+                            break;
+                        }
+                        }
+
+                    if (jogadaRealizada)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            if (campoMinado.JogoStatus == 1)
+            {
+                Console.WriteLine("Parabéns! Você venceu o Campo Minado!");
+            }
+            else if (campoMinado.JogoStatus == 2)
+            {
+                Console.WriteLine("Game Over! Você encontrou uma mina terrestre!");
+            }
+        }
+    }
+}
+
         }
     }
 }
